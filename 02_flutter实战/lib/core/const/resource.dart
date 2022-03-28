84b1977adf60060c88495aa77b30bede9bf3bbdb
zhongaidong <1042480866@qq.com>
2022-03-28 21:22:50 +0800

    【修改内容】 :  测试 【reviewer】  :  钟爱东

--- 02_flutter实战/lib/core/const/resource.dart
+++ 02_flutter实战/lib/core/const/resource.dart
@@ -5,49 +5,49 @@
 class R {
   const R._();
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/images/avatar.png)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/images/avatar.png)
   static const String assetsImagesAvatarPng = 'assets/images/avatar.png';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/01_first_app_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/01_first_app_data.json)
   static const String assetsJson01FirstAppDataJson =
       'assets/json/01_first_app_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/02_base_widgets_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/02_base_widgets_data.json)
   static const String assetsJson02BaseWidgetsDataJson =
       'assets/json/02_base_widgets_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/03_layout_widgets_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/03_layout_widgets_data.json)
   static const String assetsJson03LayoutWidgetsDataJson =
       'assets/json/03_layout_widgets_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/04_container_widgets_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/04_container_widgets_data.json)
   static const String assetsJson04ContainerWidgetsDataJson =
       'assets/json/04_container_widgets_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/05_scroll_widgets_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/05_scroll_widgets_data.json)
   static const String assetsJson05ScrollWidgetsDataJson =
       'assets/json/05_scroll_widgets_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/06_function_widgets_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/06_function_widgets_data.json)
   static const String assetsJson06FunctionWidgetsDataJson =
       'assets/json/06_function_widgets_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/07_event_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/07_event_data.json)
   static const String assetsJson07EventDataJson =
       'assets/json/07_event_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/08_animation_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/08_animation_data.json)
   static const String assetsJson08AnimationDataJson =
       'assets/json/08_animation_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/09_custom_widget_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/09_custom_widget_data.json)
   static const String assetsJson09CustomWidgetDataJson =
       'assets/json/09_custom_widget_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/10_file_network_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/10_file_network_data.json)
   static const String assetsJson10FileNetworkDataJson =
       'assets/json/10_file_network_data.json';
 
-  /// ![preview](file:///Users/zad/Documents/flutter/ad_f/02_flutter实战/assets/json/home_data.json)
+  /// ![preview](file:///Users/zad/Documents/flutter/ad_flutter/02_flutter实战/assets/json/home_data.json)
   static const String assetsJsonHomeDataJson = 'assets/json/home_data.json';
 }
